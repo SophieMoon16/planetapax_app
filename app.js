d38cc3d05db25acd4b47db356a6f6eca344177de
SophieMoon16 <sluu160593@gmail.com>
2025-08-16 13:18:23 +0200

    V1

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Définir EJS comme moteur de templates
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Servir les fichiers statiques (CSS, images...)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route principale (page d'accueil)
+app.get("/", (req, res) => {
+  res.render("index", { title: "Bienvenue chez Planetapax !" });
+});
+
+// Lancement de l'app
+app.listen(PORT, () => {
+  console.log("Application Node.js en écoute sur le port " + PORT);
+});
+
+app.use((err, req, res, next) => {
+  console.error("Erreur attrapée :", err.stack);
+  res.status(500).send(`<pre>Erreur serveur :\n${err.stack}</pre>`);
+});
---
+++ resources/js/index.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("unique-sidenav").style.width = "200px";
+}
+
+function closeNav() {
+  document.getElementById("unique-sidenav").style.width = "0";
+}
